79aaeedc2ae9c2bcfddeb51bfba938154c665418
Waldemar47 <w.karsten@tut.by>
2020-03-20 17:43:30 +0300

    init: start und create index.js

--- src/index.js
+++ src/index.js
@@ -35,12 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
-
-function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    decode
-}
+  };
+  function convert(char) {
+    let interim = "";
+    let codeMorse = ""; 
+    for(let a = 0; a < char.length; a+=2) {
+      interim = char.slice(a,a+2);
+      if (interim != "00") {
+        if (interim[1] == "*") return " "; 
+        else if (interim[1] == "0") {
+          codeMorse += '.'; 
+        }
+        else codeMorse += '-'; 
+      }
+    }
+    for(element in MORSE_TABLE) {
+      if(codeMorse == element) {
+         return MORSE_TABLE[element]; 
+      }
+    }
+  }
+  function slicer(string) {
+    let total = []; 
+    for(let b = 0; b < string.length; b+= 10) {
+      total.push(string.slice(b,b+10));
+    }
+    return total; 
+  }
+  function decode(expr) {
+    let parted = slicer(expr);
+    return parted.map(element => convert(element)).join(""); 
+  }
+  module.exports = {
+      decode
+  }
